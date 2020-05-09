4cabe3fad2fc8063174ea0f6eabbbaf80e810c72
alexith <akshay.cy2@gmail.com>
2020-05-09 16:51:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/currencypal/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.currencypal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void currencyCalc(View view) {
+
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
