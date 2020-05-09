d9c5b9a0adfe437c3bec05229691a69c13a7d944
alexith <akshay.cy2@gmail.com>
2020-05-09 17:53:11 +0530

    addition of main code to source

--- app/src/main/java/com/example/currencypal/MainActivity.java
+++ app/src/main/java/com/example/currencypal/MainActivity.java
@@ -3,14 +3,22 @@ package com.example.currencypal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public void currencyCalc(View view) {
 
+        EditText defaultCurrency = (EditText) findViewById(R.id.defaultCurrency);
 
+        int defaultCurrency = Integer.parseInt(defaultCurrency.getText().toString());
 
+        double convertedCurrency = defaultCurrency * 75.5;
+
+        Toast.makeText(MainActivity.this, "Currency in rupee: " + Double.toString(convertedCurrency), Toast.LENGTH_LONG).show();
     }
 
     @Override
